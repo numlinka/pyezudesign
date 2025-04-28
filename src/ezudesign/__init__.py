dc59a8c24445ab414e92dae9c7f91a863d15a399
numlinka <numlinka@163.com>
2025-04-28 18:29:02 +0800

    Update to version 0.2.5.

--- src/ezudesign/__init__.py
+++ src/ezudesign/__init__.py
@@ -14,7 +14,7 @@ __author__ = "numlinka"
 __license__ = "LGPLv3"
 __copyright__ = "Copyright (C) 2023 numlinka"
 
-__version_info__ = (0, 2, 4)
+__version_info__ = (0, 2, 5)
 __version__ = ".".join(map(str, __version_info__))
 
 # internal
